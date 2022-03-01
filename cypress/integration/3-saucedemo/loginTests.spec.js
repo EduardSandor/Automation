20ff3c2ccb2db0f5db9ae411e677ae4cbd4d0912
EduardSandor <sandoreduard@gmail.com>
2022-03-01 16:06:05 +0200

    added a new TC to check placeholder attribute

--- cypress/integration/3-saucedemo/loginTests.spec.js
+++ cypress/integration/3-saucedemo/loginTests.spec.js
@@ -1,8 +1,14 @@
-describe('My First Test', () => {
-  it('Visits the Sauce Demo website', () => {
+describe('Visits the Sauce Demo website', () => {
+  beforeEach(() => {
     cy.visit('https://www.saucedemo.com')
+  })
+  it('Clicks on Login button and checks the error message displayed', () => {
     cy.contains('Login').click()
     cy.get('.error').should('have.text', 'Epic sadface: Username is required')
     cy.get('.error-button').click()
+  })
+  it('Checks the placeholder text from the username and password fields', () => {
+    cy.get('input#user-name.input_error.form_input').invoke('attr', 'placeholder').should('contain', 'Username')
+    cy.get('input#password.input_error.form_input').invoke('attr', 'placeholder').should('contain', 'Password')
   })
 })
