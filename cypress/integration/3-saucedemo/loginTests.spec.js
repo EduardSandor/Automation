49b585a14a8eb172e874f549a8c03018f0306346
EduardSandor <sandoreduard@gmail.com>
2022-03-01 11:33:25 +0200

    chromeWebSecurity added into cypress.json

--- cypress/integration/3-saucedemo/loginTests.spec.js
+++ cypress/integration/3-saucedemo/loginTests.spec.js
@@ -1,6 +1,8 @@
 describe('My First Test', () => {
   it('Visits the Sauce Demo website', () => {
     cy.visit('https://www.saucedemo.com')
-    cy.contains('Login')
+    cy.contains('Login').click()
+    cy.get('.error').should('have.text', 'Epic sadface: Username is required')
+    cy.get('.error-button').click()
   })
 })
