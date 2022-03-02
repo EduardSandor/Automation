2a6a73e6a24afeeb3257e724f7ea8a9ef11617ca
EduardSandor <sandoreduard@gmail.com>
2022-03-02 18:57:53 +0200

    added a new test for an invalid username

--- cypress/integration/3-saucedemo/loginTests.spec.js
+++ cypress/integration/3-saucedemo/loginTests.spec.js
@@ -9,18 +9,26 @@ describe('Visits the Sauce Demo website', () => {
   })
   it('Checks the placeholder text from the username and password fields', () => {
     cy.get('[data-test=username]').invoke('attr', 'placeholder').should('contain', 'Username')
-    cy.get('[data-test=password').invoke('attr', 'placeholder').should('contain', 'Password')
+    cy.get('[data-test=password]').invoke('attr', 'placeholder').should('contain', 'Password')
   })
-  it('Add valid input and login', () => {
-    const loginUsername = 'standard_user'
+  it('Add input and login', () => {
+    // usernames: valid - standard_user; invalid: locked_out_user, problem_user, performance_glitch_user
+    const loginUsername = 'locked_out_user'
     const loginPassword = 'secret_sauce'
 
     cy.get('[data-test=username]').type(`${loginUsername}{enter}`)
     cy.get('[data-test=password]').type(`${loginPassword}{enter}`)
     cy.get('[data-test=login-button]').click()
-    cy.url().should('include', '/inventory.html')
-    cy.get('.bm-burger-button').click()
-    cy.get('a#logout_sidebar_link.bm-item.menu-item').click()
-    cy.url().should('not.include', '/inventory.html')
+    cy.get('[data-test="error"]').should('have.text', 'Epic sadface: Sorry, this user has been locked out.')
+    cy.get('.error-button').click()
+    cy.get('[data-test=username]').should('have.value', 'locked_out_user')
+    cy.get('[data-test=password]').should('have.value', 'secret_sauce')
+    cy.get('[data-test="error"]').should('not.exist')
+   /*
+   cy.url().should('include', '/inventory.html')
+   cy.get('.bm-burger-button').click()
+   cy.get('#logout_sidebar_link').click()
+   cy.url().should('not.include', '/inventory.html')
+    */
   })
 })
